4cabc436a3e06a53c26ddc5dd6fa2df72b7e3e75
kelovey <kelovey8@gmail.com>
2015-06-30 23:58:26 +0900

    テストだよ

--- Assets/cubetest.cs
+++ Assets/cubetest.cs
@@ -6,6 +6,7 @@ public class cubetest : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
         Debug.Log("Hello! GitHub!");
+        Debug.Log("test");
 	}
 	
 	// Update is called once per frame
